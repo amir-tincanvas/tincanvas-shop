042ec4bf85202dd5d6deca704e06ab7e5de2cfb5
Amir Hajizadeh <amir@tincanvas.com>
2020-03-16 00:11:55 -0700

    started adding local storage with redux persist for cart

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 
 import {
   selectCartItems,
-  selectCartTotal
-} from "../../redux/cart/cart.selectors";
+  selectCartTotal,
+} from '../../redux/cart/cart.selectors';
 
-import "./checkout.styles.scss";
+import './checkout.styles.scss';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -35,14 +35,14 @@ const CheckoutPage = ({ cartItems, total }) => (
     ))}
 
     <div className="total">
-      <span>TOTAL: $0</span>
+      <span>TOTAL: ${total}</span>
     </div>
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
